00d51d6703c727303dc2aedccdbbb77847b0b6da
Charawey-X <charawey@gmail.com>
2022-04-10 12:56:54 +0300

    edited to book quotes

--- src/app/quotes/quotes/quotes.component.ts
+++ src/app/quotes/quotes/quotes.component.ts
@@ -9,16 +9,30 @@ import { Quote } from '../quotes';
 export class QuotesComponent implements OnInit {
   public quotes: Quote[] = [
     <Quote>{
-      title: '15 Minute Walk',
-      description: 'Daily',
+      title: 'SKULDUGGERY PLEASANT: \nThe Dying Of The Light',
+      description: 'There\'s no such thing as winning or losing. ' +
+                    '\nThere is won and there is lost, there is victory and defeat. ' +
+                    '\nThere are absolutes. Everything in between is still left to fight for.',
       author:
-        'This habit makes my kitchen look nice and makes my day better the next morning.',
+        'Derek Landy.',
     },
     <Quote>{
-      title: 'Weed the Garden',
-      description: 'Weekly',
+      title: 'Caraval: \nBook I',
+      description: 'No one is truly honest. \nEven if we don\'t lie to others, we often lie to ourselves.',
       author:
-        'The weeds get so out of hand if they wait any longer, and I like how nice our home looks with a clean lawn.',
+        'Stephanie Garber.',
+    },
+    <Quote>{
+      title: 'Dune: \nBook I',
+      description: 'A process cannot be understood by stopping it. \nUnderstanding must move with the flow of the process, \nmust join it and flow with it.',
+      author:
+        'Frank Herbert.',
+    },
+    <Quote>{
+      title: 'The Mountains Sing',
+      description: 'If our stories survive, we will not die. \nEven when our bodies are no longer here on this Earth.',
+      author:
+        'Nguyễn Phan Quế Mai.',
     },
   ];
 
